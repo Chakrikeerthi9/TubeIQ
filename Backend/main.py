08424e81a5c2dfcb6740a20ed7bf62e5cd0ef34a
Chakrikeerthi9 <keerthichakri2148@gmail.com>
2025-10-28 19:22:47 -0500

    frontend live site url add to backend

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://tube-iq.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
